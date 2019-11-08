585be2db9339d3c806f0b465d89dac0fff54d9d2
janscheidegger <scheideggerj@gmail.com>
2019-11-08 13:46:40 +0100

    Fix test execution

--- projects/ng-time-chart/karma.conf.js
+++ projects/ng-time-chart/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../../coverage/ng-timeline'),
+      dir: require('path').join(__dirname, '../../coverage/ng-time-chart'),
       reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
     },
